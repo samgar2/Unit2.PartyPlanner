030522e852e33e57a1d2517a73f1ca99f80a5c06
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2023-11-30 14:00:18 -0800

    new workshop solutions

--- index.js
+++ index.js
@@ -1,51 +1,62 @@
 // Part 1: A user enters the website and finds a list of the names, dates, times, locations, and descriptions of all the parties that are happening
 
-// Get party elements
-const partiesContainer = document.getElementById('parties-container');
-const newPartyFormContainer = document.getElementById('new-party-form');
-
 // Variable for our API URL
 const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-GHP-ET-WEB-FT-SF/events`;
 
-// Fetch parties
-const fetchAllParties = async () => {
+// Creating our state
+let state = {
+    parties: [],
+};
+
+// Get party elements
+const partyList = document.querySelector("#parties");
+
+const addPartyForm = document.querySelector("#addParty");
+addPartyForm.addEventListener("submit", addParty);
+
+// Sync state with the API and rerender
+async function render() {
+    await getParties();
+    renderParties();
+}
+render();
+
+// GET: Update state with parties from API
+async function getParties() {
     try {
         const response = await fetch(API_URL);
-        const parties = await response.json();
-        console.log(parties);
-        return parties;
+        const json = await response.json();
+        console.log(response)
+        state.parties = json.data;
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
 }
 
-// Render all parties
-const renderAllParties = (partyList) => {
-    if (!partyList || partyList.length === 0) {
-        partiesContainer.innerHTML = '<h3>No parties found</h3>';
+// Render parties from state
+function renderParties() {
+    console.log(state.parties)
+    if (!state.parties.length) {
+        partyList.innerHTML = "<li>No parties.</li>";
         return;
     }
 
-    partiesContainer.innerHTML = '';
-
-    partyList.data.forEach((party) => {
-        console.log(party);
-        const partyElement = document.createElement('div');
-        partyElement.classList.add('party-card');
-
-        partyElement.innerHTML = `
+    const partyCards = state.parties.map((party) => {
+        const li = document.createElement("li");
+        li.innerHTML = ` 
             <h4>Name: ${party.name}</h4>
             <p>${getdateTime(party.date)}</p>
             <p>Location: ${party.location}</p>
             <p>Description: ${party.description}</p>
-        `;
-
-        partiesContainer.appendChild(partyElement);
+      `;
+        return li;
     });
+
+    partyList.replaceChildren(...partyCards);
 }
 
 // Separate out the date and time
-function getdateTime (dateTimeString) {
+function getdateTime(dateTimeString) {
     let date = ''
     let time = ''
 
@@ -60,9 +71,37 @@ function getdateTime (dateTimeString) {
     return `Date: ${date} Time: ${time}`
 }
 
-const init = async () => {
-    const parties = await fetchAllParties();
-    renderAllParties(parties);
+ // POST: Ask the API to create a new artist based on form data
+async function addParty(event) {
+    event.preventDefault();
+  
+    try {
+    let DateAndTime = `${event.target[1].value}T${event.target[2].value}Z`
+
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: addPartyForm.name.value,
+          date: DateAndTime,
+          location: addPartyForm.location.value,
+          description: addPartyForm.description.value,
+        }),
+      });
+  
+      if (!response.ok) {
+        throw new Error("Failed to create party");
+      }
+  
+      render();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const init = async () => {
+    const parties = await getParties();
+    renderParties(parties);
 }
 
 init();
